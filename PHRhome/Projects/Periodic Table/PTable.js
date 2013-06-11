0143c40cbbe0ed594a810c5fd63365c1edde7627
T.J.K. <tjk16384@gmail.com>
2013-06-11 12:48:24 -0400

    basic PTable functionality now working

--- PHRhome/Projects/Periodic Table/PTable.js
+++ PHRhome/Projects/Periodic Table/PTable.js
@@ -1,4 +1,7 @@
-﻿$(document).ready(function ()
+﻿var sStyle = "PTable.css";
+
+
+$(document).ready(function ()
 {
     AddStyle();
     PTableCalc();
@@ -9,48 +12,176 @@
 function AddStyle()
 {
     // http://stackoverflow.com/questions/10665268/correct-way-to-include-css-after-head
-    
+    /*
     linkElement = document.createElement("link");   //create a new <link> node
     linkElement.rel = "stylesheet";
     linkElement.href = "./PTable.css";
     
     document.head.appendChild(linkElement);         //append the newly-created <link> to the <head>
+    */
+
+    // The jQuery way:
+    // http://rickardnilsson.net/post/2008/08/02/Applying-stylesheets-dynamically-with-jQuery.aspx
+
+    $('head').append('<link rel="stylesheet" type="text/css" href="' + sStyle + '" />');
 }
 
 
 function PTableCalc()
 {
     /*
-        IDEA:  Since the elements are grouped into blocks, append them into DOM blocks
+        IDEA:  Since the elements are grouped into blocks, append them as DOM blocks
     */
+    
+    var $Elem = $("#testElem");
 
     var arcBlock = ["s", "p", "d", "f", "g"];
-    var ar$Block = new Array(); //an array to be filled with DOM nodes
-    var JPeriod = {
-        1: 2,
-        2: 8,
-        3: 8,
-        4: 18,
-        5: 18,
-        6: 32,
-        7: 32,
-        8: 50,
-        9: 50
-    };
-
+    var ar$Block = new Array(); //an array to be filled with PT blocks
+    var ar$Elem = new Array();  //to be filled with the Elements
+    
     var arnBlock = new Array();
     var arnPeriod = new Array();
     var B = arcBlock.length;  //blocks
-    var P = 9;  //periods
-    var b, p;
+    var P = 7;  //periods
+    var b, p, x;
     
     //calculate block widths:
     for (b = 0; b < B; b++) {
         arnBlock[b] = b * 4 + 2;    //2, 6, 10, 14, 18, ...
-        ar$Block[b] = $("<div id=\"block-" + arcBlock[b] + "\"></div>");
-        $("#PTable").appendTo();
+
+        ar$Block[b] = $("<div id='block-" + arcBlock[b] + "'></div>");
+        ar$Block[b].width( arnBlock[b] * 84 );
+        ar$Block[b].appendTo("#PTable");
+    }
+
+    //set block positions:
+    $("#block-s")
+        .css("left", 0)
+        .css("top", 0)
+    ;
+    $("#block-p")
+        .css("left", 84 * 12)
+        .css("top", 88 * 1)
+    ;
+    $("#block-d")
+        .css("left", 84 * 2)
+        .css("top", 88 * 3)
+    ;
+    $("#block-f")
+        .css("left", 84 * 2)
+        .css("top", 88 * 7 + 16)
+    ;
+
+    //set table width/height:
+    $("#PTable")
+        .width(84 * 18)
+        .height(88 * 10)
+    ;
+    
+
+    /*
+    var nStyleIndex = -1;
+    var sheet;
+    var arsCSS = new Array();
+
+    // use a loop to specifically grab the DOM for "PTable.css":
+    for (x = 0; x < document.styleSheets.length; x++) {
+        if ( document.styleSheets[x].href.indexOf(sStyle) >= 0 ) {  //does this stylesheet's href contain the string "PTable.css"?
+            sheet = document.styleSheets[x];
+            //nStyleIndex = x;
+            break;
+        }
+    }
+    */
+
+    //$Elem.height("80");
+    //$Elem.find(".weight").html($Elem.height());
+    $Elem.detach();
+    
+    //NOTE: getJSON is an async, and has no return values; also, run it last
+    $.getJSON("./elements.json")
+        .done(GotPData)
+        .fail(GotPError)
+    ;
+    
+
+    //run when JSON request succeeds:
+
+    function GotPData(data, textStatus, jqXHR) {
+        //$("#loadJSON").html("Successfully imported JSON.");
+        $("#loadJSON").remove();
+        
+        var PTable = data.Elements;
+        var e, E = PTable.length;
+        
+        for (e = 0; e < E; e++) {
+            ar$Elem[e] = $Elem.clone(); //make a duplicate of template "Test Element" DOM node
+
+            ar$Elem[e].attr("id",PTable[e].symbol);
+
+            //apply the JSON data:
+            ar$Elem[e].find(".name")
+                .html(PTable[e].name)
+            ;
+            ar$Elem[e].find(".num")
+                .html(PTable[e].atomic_number)
+                .addClass( whichState(PTable[e]) )
+            ;
+            ar$Elem[e].find(".symbol")
+                .html(PTable[e].symbol)
+                .addClass(PTable[e].classes)
+            ;
+            ar$Elem[e].find(".weight")
+                .html(PTable[e].atomic_weight)
+            ;
+            //
+
+            //ar$Elem[e].appendTo("#PTable");
+            ar$Elem[e].appendTo("#block-" + PTable[e].block);
+        }
+
+        //reposition Helium:
+        var $He = $("#He").clone();
+        $("#He").first().css("visibility","hidden");
+        $He.prependTo("#PTable")
+            .css("left", 84 * 17)
+            .css("top", 0)
+        ;
     }
     
-    //
+}
+
+/*
+function GetPData() {
+    return $.getJSON("./elements.json", function (data) { return data; })
+        //.done(GotPData)
+        //.fail(GotPError)
+        //.always(function (jqXHR, textStatus, errorThrown) { $("#loadJSON").html( textStatus ) })
+    ;
+}
+*/
+
+function GotPError(jqXHR, textStatus, errorThrown) {
+    var str = "Failure: " + errorThrown;
+    $("#loadJSON").html(str);
+    //return str;
+}
+
+
+//"whichState": calculates the element's matter phase (solid, liquid, gas) @ STP
+function whichState(JElem) {
+    if (JElem["boiling_point K"] == "n/a" || JElem["melting_point K"] == "n/a") {
+        return;
+    }
+
+    if (JElem["boiling_point K"] < 273) {
+        return "Gas";
+    }
+    else if (JElem["melting_point K"] < 273) {
+        return "Liquid";
+    }
+    else {
+        return "Solid";
+    }
 }
 
--- PHRhome/Scripts/Main.js
+++ PHRhome/Scripts/Main.js
@@ -4,53 +4,65 @@
     
     //alert(window.innerWidth + "px * " + window.innerHeight + "px.")
     
-    //store jQuery object-grabbers into vars starting with "$":
-    var $Time = $("#timespan");
-    var $WarnScr = $("#warnScreen");
-    
     //stuff to run repeatedly:
     setInterval(function () {
         getTime();
         checkViewSize();
     }, 143);
+});
+
+
+function getTime() {
+    /*  http://stackoverflow.com/questions/1531093/how-to-get-current-date-in-javascript  */
+
+    //store jQuery object-grabbers into vars starting with "$":
+    var $Time = $("#timespan");
     
-    
-    function getTime() {
-        /*  http://stackoverflow.com/questions/1531093/how-to-get-current-date-in-javascript  */
+    var MONTH = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-        var MONTH = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var Today = new Date();
 
-        var Today = new Date();
+    var YYYY = Today.getFullYear();
+    var DD = Today.getUTCDate();
+    var MM = Today.getUTCMonth(); //fyi: january is 0
 
-        var YYYY = Today.getFullYear();
-        var DD = Today.getUTCDate();
-        var MM = Today.getUTCMonth(); //fyi: january is 0
+    var hr = Today.getUTCHours();
+    var min = Today.getUTCMinutes();
+    var sec = Today.getUTCSeconds();
 
-        var hr = Today.getUTCHours();
-        var min = Today.getUTCMinutes();
-        var sec = Today.getUTCSeconds();
+    if (DD < 10) { DD = '0' + DD }
+    //if (MM < 10) { MM = '0' + MM }    //is an index int, no longer a string
+    if (hr < 10) { hr = '0' + hr }
+    if (min < 10) { min = '0' + min }
+    if (sec < 10) { sec = '0' + sec }
 
-        if (DD < 10) { DD = '0' + DD }
-        //if (MM < 10) { MM = '0' + MM }    //is an index int, no longer a string
-        if (hr < 10) { hr = '0' + hr }
-        if (min < 10) { min = '0' + min }
-        if (sec < 10) { sec = '0' + sec }
+    Today = YYYY + '/' + MONTH[MM] + '/' + DD + "  " + hr + ":" + min + ":" + sec + " UTC";
 
-        Today = YYYY + '/' + MONTH[MM] + '/' + DD + "  " + hr + ":" + min + ":" + sec + " UTC";
+    $Time.html(Today);
+}
 
-        $Time.html(Today);
+function checkViewSize() {
+    var W = window.innerWidth;
+    var H = window.innerHeight;
+
+    var $WarnScr = $("#warnScreen");
+
+    $WarnScr.html(W + "px W * " + H + "px H");
+
+    if (W < 1000 || H < 600) {
+        $WarnScr.append("<br /><span class='Red'>The viewport may be too small to display this site properly.  Please resize the window, hide one or more browser toolbars, or increase your screen resolution.</span>");
     }
+}
 
-    function checkViewSize() {
-        var W = window.innerWidth;
-        var H = window.innerHeight;
-        
-        $WarnScr.html( W + "px W * " + H + "px H" );
-        
-        if (W < 1000 || H < 600) {
-            $WarnScr.append("<br /><span class='R'>The viewport may be too small to display this site properly.  Please resize the window, hide one or more browser toolbars, or increase your screen resolution.</span>");
+
+/// http://stackoverflow.com/questions/16873323/javascript-sleep-wait-before-continuing
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds) {
+            break;
         }
     }
-    
-});
+}
 
